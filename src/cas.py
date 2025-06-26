f82cd472962f42b0b5e12a88715c306711952a29
Brian Christner <atech@miloPC.hsd1.tx.comcast.net>
2025-06-25 23:05:15 -0500

    Fixing sympy matrix issues

--- src/cas.py
+++ src/cas.py
@@ -1,13 +1,39 @@
-from sympy import simplify
+from sympy import simplify, parse_expr
 from latex2sympy2_extended import latex2sympy
 from sympy.printing.latex import latex
 
 import js
 
+variables = dict() # Store variables set in current context
+
+def from_variable_format(st):
+    # Replace variable format to parsable variables in sympy
+    for variable in variables:
+        st = st.replace(f"\\\\var{{{variable}}}", f"({variables[variable]})")
+
+    return st
+
 def sympify(latex_string):
-    # Convert latex_string to sympy, then "doit" and execute.
+    # Convert latex_string to sympy, then parse expr with evaluation set to true, doit, and simplify!
     # Return as latex code of result
-    val = simplify(latex2sympy(latex_string).doit())
-    return str(latex(val))
+
+    latex_string = from_variable_format(latex_string)
+    
+    variable = ""
+    if ":=" in latex_string:
+        # We are setting a variable value
+        # Cut off the assignment, and we will assign after.
+        variable, latex_string = latex_string.split(":=")
+
+    sympy_form = str(latex2sympy(latex_string))
+    val = simplify(parse_expr(sympy_form, evaluate=True).doit())
+
+    str_latex = str(latex(val))
+
+    if variable != "":
+        # We should save this variable
+        variables[variable] = str_latex
+
+    return str_latex
 
 js.sympify = sympify
