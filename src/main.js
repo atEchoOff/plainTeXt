e7939601be7206bdb7964bc3bb92003b1428d65b
atEchoOff <at.echo.off@outlook.com>
2025-07-20 15:54:13 -0500

    Adding permanent dark mode

--- src/main.js
+++ src/main.js
@@ -729,7 +729,14 @@ const darkModeButton = document.getElementById("dark-mode-button");
 darkModeButton.addEventListener("click", () => {
     if (document.body.classList.contains("dark")) {
         document.body.classList.remove("dark");
+        localStorage.setItem("darkmode", false)
     } else {
         document.body.classList.add("dark");
+        localStorage.setItem("darkmode", true);
     }
-})
+});
+
+// Set darkmode to applicable item
+if (localStorage.getItem("darkmode") === "true") {
+    document.body.classList.add("dark");
+}
